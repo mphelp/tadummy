becaee5f053e327b55e4cb140690b4ab728df6c8
Matthew Phelps <mphelps3@nd.edu>
2019-02-05 11:25:53 -0500

    edit before removing base nodemodules

--- server/index.js
+++ server/index.js
@@ -2,4 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 
+app.get('/', (req, res) => {
+	res.send('Hello there welcome to TAdummy - Ed, Matt')
+})
 app.listen(port, () => console.log(`server running on port ${port}`))
