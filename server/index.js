cd087a8a35f3fa4a45f8e43a4cf6fb095475b4a2
Matthew Phelps <mphelps3@nd.edu>
2019-02-22 15:26:12 -0500

    included Patrick :)

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ const app = express()
 const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
-	res.send('Hello there welcome to TAdummy - Ed, Matt')
+	res.send('Hello there welcome to TAdummy - Ed, Matt, and Patrick!')
 })
 app.listen(port, () => console.log(`server running on port ${port}`))
