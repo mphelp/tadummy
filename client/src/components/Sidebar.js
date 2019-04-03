fbd82528021236cbe12bc84dc2665e52f4343573
Matthew Phelps <mphelps3@nd.edu>
2019-04-03 14:15:46 -0400

    starting sidebar

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SideBarGeneral_s = {
+    maxWidth: "25%"
+}
+export default class extends React.Component {
+    render(){
+        return (
+            <React.Fragment>
+                This is the sidebar
+            </React.Fragment>
+        )
+    }
+}
