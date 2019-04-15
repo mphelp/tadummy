4791ec26cc793ae4a0e495d34643f5ce19795736
edat44 <eatkins1@nd.edu>
2019-04-15 20:49:14 +0000

    fixed CRTdorm error

--- database/tableCreation/CRTdorm.sql
+++ database/tableCreation/CRTdorm.sql
@@ -1,12 +1,6 @@
 drop table dorm cascade constraints;
 
-create or replace sequence seq_dorm
-    MINVALUE 1
-    START WITH 1
-    INCREMENT BY 1
-    CACHE 10;
-
 create table dorm(
-    dorm_id     number(3) PRIMARY KEY DEFAULT seq.dorm.nextval not null,
-    dorm_name   varchar2(30),
+    dorm_id     number(3) PRIMARY KEY,
+    dorm_name   varchar2(30)
 );
