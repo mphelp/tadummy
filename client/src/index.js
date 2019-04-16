ba8af7c1a45ad0fcec13a1778638c592b685ec66
Matthew Phelps <mphelps3@nd.edu>
2019-04-16 18:07:50 +0000

    first steps redirection

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+const yeah = require('config.js')
+console.log(yeah.port)
 
+// Redirect to server for cas
+window.location.href="localhost:" + yeah.port;
+
+// Render app
 render(<App />, document.getElementById('root'));
 
