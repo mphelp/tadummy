b661cc4e273a6bce04f653472f580e7a5d36d506
Matthew Phelps <mphelps3@nd.edu>
2019-04-16 19:56:20 +0000

    successfully post to client from server. now?

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ app.all('*', cas.bounce);
 
 app.get('/', auth.authorize([auth.ROLES.ADMIN], cas), (req, res) => {
     //res.json(req.session);
-		res.sendfile('redirection/index.html');
+		res.sendFile('redirection/index.html');
 })
 
 app.get('/signup', (req, res) => {
--- server/redirection/main.js
+++
@@ -1,8 +0,0 @@
-
-input = new Input()
-form  = new Form()
-
-input.createInput();
-form.createForm("theform", "35.153.114.63", "8009");
-input.addTo(form);
-form.addToDocument();
