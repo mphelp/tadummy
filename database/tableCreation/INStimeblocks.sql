fc348bcfe26a09615b7312197bf01502326871a4
Edward Atkinson (Group VM) <eatkins1@nd.edu>
2019-04-24 17:40:46 +0000

    timestamp keyword needed

--- database/tableCreation/INStimeblocks.sql
+++ database/tableCreation/INStimeblocks.sql
@@ -1,3 +1,3 @@
-insert into timeblock (timeblock_id, location, starttime, endtime) values (100, 'LaDunc Innovation Lounge', '2019-04-24 15:30:00.00', '2019-04-24 19:45:00.00');
+insert into timeblock (timeblock_id, location, starttime, endtime) values (100, 'Innovation Lounge', TIMESTAMP '2019-04-24 15:30:00.00', TIMESTAMP '2019-04-24 19:45:00.00');
 
-insert into timeblock (timeblock_id, location, starttime, endtime) values (101, 'LaDunc Innovation Lounge', '2019-04-25 12:30:00.00', '2019-04-24 19:15:00.00');
+insert into timeblock (timeblock_id, location, starttime, endtime) values (101, 'Innovation Lounge', TIMESTAMP '2019-04-25 12:30:00.00', TIMESTAMP '2019-04-24 19:15:00.00');
