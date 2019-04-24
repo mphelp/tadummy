83fbb37d8af3a2aa376121d0c3eb2daff5a5a00c
Patrick Falvey <pfalvey@nd.edu>
2019-04-24 18:25:52 +0000

    add signup forms for student/faculty members

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -10,13 +10,49 @@ import "@blueprintjs/select/lib/css/blueprint-select.css";
 // My CSS
 import './App.css';
 
-const Home = (props) => (
+const HomeFaculty = (props) => (
     <div>
-        <h1>Hello {props.netid}, please sign up!</h1>
-        {JSON.stringify(props)}
+        <h1>Hello Professor {props.displayname}, please sign up!</h1>
+        <br/>
+        <form>
+        Name:<br />
+        <input type="text" name="name" value={props.displayname}/><br/><br/>
+        NetID:<br />
+        <input type="text" name="netid" value={props.netid}/><br/><br/>
+        Department:<br />
+        <input type="text" name="department" value={props.nddepartment}/><br/><br/>
+        Office:<br />
+        <input type="text" name="office" value={props.ndofficeaddress}/><br/><br/>
+        <input type="submit" value="Submit" />
+        </form>
+        {/*JSON.stringify(props)*/}
     </div>
 )
 
+const HomeStudent = (props) => (
+    <div>
+        <h1>Hello {props.displayname}, please sign up!</h1>
+        <br />
+        <form>
+        Name:<br />
+        <input type="text" name="name" value={props.displayname}/><br/><br/>
+        NetID:<br />
+        <input type="text" name="netid" value={props.netid}/><br/><br/>
+        Major:<br />
+        <input type="text" name="major" /><br/><br/>
+        Dorm (or type "Off Campus"):<br />
+        <input type="text" name="dorm" /><br/><br/>
+        <input type="submit" value="Submit" />
+        </form>
+        {/*JSON.stringify(props)*/}
+    </div>
+)
+
+const HomeError = (props) => (
+    <div>
+        <h1>Error: No student or faculty member found with NetID {props.netid}.</h1>
+    </div>
+)
 const BodyGeneral_s = {
     display: "flex",
     flexDirection: "row",
@@ -35,7 +71,24 @@ export default class extends React.Component {
   render() {
 		return (
                 <div style={BodyGeneral_s}>
-                    <Home {...this.props} />
+                {   (() => {
+                        if (this.props.ndprimaryaffiliation == "Faculty"){
+                            return(
+                            <HomeFaculty {...this.props} />
+                            )
+                        }
+                        if (this.props.ndprimaryaffiliation == "Student"){
+                            return(
+                            <HomeStudent {...this.props} />
+                            )
+                        }
+                        else {
+                            return(
+                            <HomeError {...this.props} />
+                            )
+                        }
+                    })()
+                }
                 </div>
     );
   }
