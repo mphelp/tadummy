a879e34017479e16470d0756b1204453a67e93f1
edat44 <eatkins1@nd.edu>
2019-04-25 17:35:12 +0000

    added userroles view

---
+++ database/tableCreation/CRTuserroles.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE VIEW userroles AS
+    SELECT
+        u.netid AS netid,
+        u.admin AS admin,
+        CASE
+		    WHEN s.netid IS NULL THEN 0
+            ELSE 1
+  	    END AS student,
+	    CASE
+ 		    WHEN p.netid IS NULL THEN 0
+ 		    ELSE 1
+        END AS professor
+    FROM users u
+         FULL OUTER JOIN student s ON u.netid = s.netid
+ 	     FULL OUTER JOIN professor p ON u.netid = p.netid
+/
