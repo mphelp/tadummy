851da3ea87e92de09b9f52544270c1d58e7e3198
edat44 <eatkins1@nd.edu>
2019-04-27 21:19:19 +0000

    added student and ta enrollment functionality

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -33,6 +33,54 @@ router.get('/', api.query(getAllCourses));
 
 router.get('/:cid', api.query(getCourseReq));
 
+router.post('/enroll', (req, res) => {
+    if (missingKeys(req.body, ['netid', 'courses'], req, res).length) {
+        return;
+    }
+    let {netid, courses} = req.body;
+    let promises = [];
+    for (i in courses) {
+        let cid = courses[i];
+        console.log(cid);
+        promises.push(enrollCourse(cid, netid));
+    }
+    return Promise.all(promises).then( (result) => {
+        console.log(result);
+        res.sendStatus(201);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+router.post('/enrollta', (req, res) => {
+    if (missingKeys(req.body, ['netid', 'cid'], req, res).length) {
+        return;
+    }
+    let {netid, cid} = req.body;
+    return enrollTACourse(cid, netid).then( result => {
+        console.log(result);
+        res.sendStatus(201);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+function enrollCourse(cid, netid) {
+    let sql = `
+        insert into admin.studentfor(netid, course_id)
+        values (:netid, :cid)
+    `;
+    return db.queryDB(sql, [netid, cid], db.QUERY.INSERT);
+}
+
+function enrollTACourse(cid, netid) {
+    let sql = `
+        insert into admin.tafor(netid, course_id, avail_id)
+        values (:netid, :cid, 1)
+    `;
+    return db.queryDB(sql, [netid, cid], db.QUERY.INSERT);
+}
+
 function getAllCourses() {
     return db.queryDB("SELECT * FROM admin.course", [], db.QUERY.MULTIPLE);
 }
