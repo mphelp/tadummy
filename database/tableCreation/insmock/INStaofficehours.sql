16e83299eb3e519ec23492dca7176a8f973b57d8
edat44 <eatkins1@nd.edu>
2019-04-29 20:09:41 +0000

    fixed spelling mistake

--- database/tableCreation/insmock/INStaofficehours.sql
+++ database/tableCreation/insmock/INStaofficehours.sql
@@ -1,8 +1,6 @@
-insert into tafor(netid, course_id, avail_id, status)
-    values('mphelps3', 2, 1, 'I am here');
+insert into tafor(netid, course_id, avail_id, status) values('mphelps3', 2, 1, 'I am here');
 
-insert into tafor(netid, course_id, avail_id, status)
-    values('pfavley', 1, 1, 'I am there');
+insert into tafor(netid, course_id, avail_id, status) values('pfalvey', 1, 1, 'I am there');
 
 insert into taofficehours (timeblock_id, netid, course_id) values (1, 'mphelps3', 2);
 insert into taofficehours (timeblock_id, netid, course_id) values (2, 'mphelps3',  2);
