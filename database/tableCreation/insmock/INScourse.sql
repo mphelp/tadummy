3c21d7be8e9213cb494408a00edddad82fe9cae4
edat44 <eatkins1@nd.edu>
2019-04-29 19:59:12 +0000

    Added more mock data

--- database/tableCreation/insmock/INScourse.sql
+++ database/tableCreation/insmock/INScourse.sql
@@ -4,3 +4,5 @@ insert into course (course_name,semester_id) values ('Programming Challenges', 1
 insert into course (course_name,semester_id) values ('Programming Paradigms', 1);
 insert into course (course_name,semester_id) values ('Systems Programming', 2);
 
+insert into coursetime (course_id, timeblock_id) values (2, 5);
+
--- database/tableCreation/insmock/INStaofficehours.sql
+++ database/tableCreation/insmock/INStaofficehours.sql
@@ -1,9 +1,10 @@
 insert into tafor(netid, course_id, avail_id, status)
-    values('eatkins1', 1, 1, 'I am here');
+    values('mphelps3', 2, 1, 'I am here');
 
-insert into taofficehours (timeblock_id, netid, course_id) values (1, 'mphelps3', 1);
-insert into taofficehours (timeblock_id, netid, course_id) values (2, 'pfalvey',  2);
-insert into taofficehours (timeblock_id, netid, course_id) values (3, 'eatkins1', 1);
-insert into taofficehours (timeblock_id, netid, course_id) values (4, 'eatkins1', 1);
-insert into taofficehours (timeblock_id, netid, course_id) values (5, 'jbao',     5);
+insert into tafor(netid, course_id, avail_id, status)
+    values('pfavley', 1, 1, 'I am here');
+
+insert into taofficehours (timeblock_id, netid, course_id) values (1, 'mphelps3', 2);
+insert into taofficehours (timeblock_id, netid, course_id) values (2, 'mphelps3',  2);
+insert into taofficehours (timeblock_id, netid, course_id) values (3, 'mphelps3', 2);
 
