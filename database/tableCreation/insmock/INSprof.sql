b6d8d0b314c5e3ad5666e21c1802765ea0b930b3
edat44 <eatkins1@nd.edu>
2019-04-29 20:20:32 +0000

    removed unused availabilities

--- database/tableCreation/insmock/INSprof.sql
+++ database/tableCreation/insmock/INSprof.sql
@@ -1,6 +1,6 @@
 insert into users (netid, name, admin) values ('pbui', 'Peter Bui', 0);
 
 insert into professor (netid, department_id) values ('pbui', 1);
-insert into proffor(netid, course_id, avail_id) values ('pbui', 2, 2);
+insert into proffor(netid, course_id, avail_id) values ('pbui', 2, 3);
 insert into profofficehours (timeblock_id, netid, course_id) values (4, 'pbui', 2);
 
