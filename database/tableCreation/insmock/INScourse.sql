d5d23f840bd7e0c0f22b657b630c3f96235b0084
Edward Atkinson (Group VM) <eatkins1@nd.edu>
2019-04-30 05:32:28 +0000

    add mock data

--- database/tableCreation/insmock/INScourse.sql
+++ database/tableCreation/insmock/INScourse.sql
@@ -1,8 +1,5 @@
 insert into course (course_name,semester_id) values ('Computer Architecture', 1);
-insert into course (course_name,semester_id) values ('Operating Systems Principles', 2);
+insert into course (course_name,semester_id) values ('Operating Systems Principles', 1);
 insert into course (course_name,semester_id) values ('Programming Challenges', 1);
 insert into course (course_name,semester_id) values ('Programming Paradigms', 1);
-insert into course (course_name,semester_id) values ('Systems Programming', 2);
-
-insert into coursetime (course_id, timeblock_id) values (2, 5);
-
+insert into course (course_name,semester_id) values ('Systems Programming', 1);
--- database/tableCreation/insmock/INSprof.sql
+++ database/tableCreation/insmock/INSprof.sql
@@ -1,6 +1,21 @@
 insert into users (netid, name, admin) values ('pbui', 'Peter Bui', 0);
-
 insert into professor (netid, office, department_id) values ('pbui', '350 Fitzpatrick Hall Of Engineering$Notre Dame, IN 46556', 1);
-insert into proffor(netid, course_id, avail_id, status) values ('pbui', 2, 3, 'In class');
-insert into profofficehours (timeblock_id, netid, course_id) values (4, 'pbui', 2);
 
+insert into users (netid, name, admin) values ('skumar5', 'Shreya Kumar', 0);
+insert into professor (netid, office, department_id) values ('skumar5', '378 Fitzpatrick Hall Of Engineering$Notre Dame, IN 46556', 1);
+
+insert into users (netid, name, admin) values ('jbb', 'Jay Brockman', 0);
+insert into professor (netid, office, department_id) values ('jbb', '211A Cushing Hall of Engineering$Notre Dame, IN 46556', 1);
+
+
+insert into proffor(netid, course_id, avail_id, status) values ('pbui', 2, 2, 'In class');
+insert into proffor(netid, course_id, avail_id, status) values ('pbui', 3, 3, '');
+insert into proffor(netid, course_id, avail_id, status) values ('pbui', 5, 3, '');
+insert into proffor(netid, course_id, avail_id, status) values ('skumar5', 4, 1, 'In my office');
+insert into proffor(netid, course_id, avail_id, status) values ('jbb', 1, 2, 'At lunch');
+
+
+
+insert into profofficehours (timeblock_id, netid, course_id) values (10, 'pbui', 2);
+insert into profofficehours (timeblock_id, netid, course_id) values (11, 'skumar5', 4);
+insert into profofficehours (timeblock_id, netid, course_id) values (12, 'jbb', 1);
