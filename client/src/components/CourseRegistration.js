b61e68ae4879e689d10d174addfdb1bf3b0fb005
Matthew Phelps <mphelps3@nd.edu>
2019-04-30 05:35:05 +0000

    waiting for route to complete course create

--- client/src/components/CourseRegistration.js
+++ client/src/components/CourseRegistration.js
@@ -3,30 +3,61 @@ import axios from 'axios';
 import {
     Classes,
     MenuItem,
-    Button
+    Button,
+    InputGroup,
+    Tag,
 } from "@blueprintjs/core";
 import { Select } from "@blueprintjs/select";
+import moment from 'moment';
+import TimePicker from 'rc-time-picker';
+import 'rc-time-picker/assets/index.css';
 
-
-// Framework CSS
-import "normalize.css";
-import "@blueprintjs/core/lib/css/blueprint.css";
-import "@blueprintjs/icons/lib/css/blueprint-icons.css";
-import "@blueprintjs/select/lib/css/blueprint-select.css";
-
+const DaysOfWeek = require('./DaysOfWeek.js');
+const nowMoment = moment().hour(0).minute(0);
+const format = 'h:mm a';
 
 const $ = require('jquery');
 const config = require('../config.js')
 const serverUrl = 'http'+(config.server.https ?'s':'')+'://'+ config.ip + ':' + config.server.port;
 
+// Styles
+const general_s = {
+    margin: 20,
+}
+const bottom_s = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+}
+const time_s = {
+		maxWidth: 100,
+		width: 100,
+		margin: "auto",
+}
+const timeblock_s = {
+    background: '#DDEEF0',
+    padding: 15,
+    marginBottom: 20,
+}
+const days_s = {
+    height: 30,
+    width: "100%",
+    background: '#DDDDEE',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    padding: 5,
+    marginBottom: 10,
+}
+const day_s = {
+    display: 'flex',
+    marginRight: 5,
+}
 const BodyGeneral_s = {
     display: "flex",
     flexDirection: "row",
     height: "100%"
 }
-
-//course NAME
-//department name ~ id
 
 export default class extends React.Component {
     constructor(props){
@@ -36,36 +67,46 @@ export default class extends React.Component {
 		search: null,
 		error: null,
 		isLoaded: false,
-        netid: "",
+    netid: "",
 		depts: [],
-        sems: [],
+    sems: [],
 		dept: null,
-        sem: null
+    sem: null,
+    timesChosen: [],
 	};
 
 	handleSubmit = (event) => {
 			event.preventDefault();
 			const formData = new FormData(event.target);
-			var object = {};
+      const { timesChosen } = this.state;
+      if (!timesChosen){
+          return;
+      }
+			let body = {};
+      let times = [];
 			formData.forEach(function(value, key){
-					object[key] = value;
+					body[key] = value;
 			});
-            object['dept'] = this.state.dept.DEPARTMENT_ID;
-            object['semester'] = this.state.sem.SID;
-            object['netid'] = this.props.netid;
-			var json = JSON.stringify(object);
-			fetch(serverUrl+'/api/courses', {
-					method: 'POST',
-					body: json,
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-			})
+      body['dept'] = this.state.dept.DEPARTMENT_ID;
+      body['semester'] = this.state.sem.SID;
+      body['netid'] = this.props.netid;
+      // Times
+      timesChosen.forEach(time => {
+          times.push({
+              starttime: time.starttime,
+              endtime: time.endtime,
+              location: time.location,
+          })
+      });
+      body['times'] = times;
+      console.log(body);
+			axios.post('/api/courses', body)
             .then(() => alert("Course created."))
             .catch(err => alert(err))
+      this.removeTimesChosen();
 	}
 
-	initializeSignup = () => {
+	initialize = () => {
 		// server routes
 		let deptApi = serverUrl + "/api/departments";
         let semApi = serverUrl + "/api/semesters";
@@ -83,12 +124,48 @@ export default class extends React.Component {
     		.catch(err => console.error(err))
 	}
 	componentWillMount() {
-		this.initializeSignup();
+		this.initialize();
 	}
-
-	handleDeptSelectClick = (dept) => {
+  // Handles
+	handleDeptSelectClick = dept => {
 			this.setState({ dept })
 	}
+	handleDaySelectClick = day => {
+			this.setState({ day });
+	}
+  handleDayStart = start => {
+      this.setState({ start });
+  }
+  handleDayEnd = end => {
+      this.setState({ end });
+  }
+  removeTimesChosen = () => {
+      this.setState({ timesChosen: [] })
+  }
+  handleChangeLocation = event => {
+      this.setState({ location: event.target.value });
+  }
+  handleAddHours = () => {
+    let properStart = this.state.start.clone();
+    let properEnd   = this.state.end.clone();
+    if (!this.state.day || !this.state.end || !this.state.start || !this.state.location){
+      return;
+    }
+    properStart.day(this.state.day.MOMENT.day());
+    properEnd.day(this.state.day.MOMENT.day());
+    let newBlock = {
+      starttime: properStart,
+      endtime:   properEnd,
+      location: this.state.location,
+      id: properStart.format()+properEnd.format()
+    };
+    if (this.state.timesChosen.find(time => {
+        const { starttime, endtime } = time;
+        return properStart.isBetween(starttime, endtime, null, '[]') || properEnd.isBetween(starttime, endtime, null, '[]');
+    }) === undefined){
+      this.setState({ timesChosen: [...this.state.timesChosen, newBlock] })
+    }
+  }
 
 	deptRenderer = (item, { handleClick, isActive }) => (
 		 <MenuItem
@@ -96,6 +173,14 @@ export default class extends React.Component {
 				 key={item.DEPARTMENT_ID}
 				 text={item.NAME}
 				 label={item.ABBREV}
+				 onClick={handleClick}
+		 />
+	)
+  dayRenderer = (item, { handleClick, isActive }) => (
+		 <MenuItem
+				 className={ isActive ? Classes.ACTIVE : "" }
+				 key={item.TITLE}
+				 text={item.TITLE}
 				 onClick={handleClick}
 		 />
 	)
@@ -136,19 +221,16 @@ export default class extends React.Component {
 
 
   render() {
-		const {
-            netid,
-			nddepartment,
-		} = this.props;
-		const { depts, dept, sems, sem} = this.state;
+		const { netid, nddepartment } = this.props;
+		const { timesChosen, day, depts, dept, sems, sem} = this.state;
 		return (
-            <div>
+            <div style={general_s}>
                     <h1>Course Registration: please enter in the following information</h1>
                     <div style = {BodyGeneral_s}>
                     <br />
                     <form onSubmit={this.handleSubmit}>
                     Course Name:<br />
-                    <input type="text" name="name" required="required"/><br/><br/>
+                    <input type="text" name="cname" required="required"/><br/><br/>
                     Department:<br />
                     <Select
                             items={depts}
@@ -168,9 +250,76 @@ export default class extends React.Component {
                             onItemSelect={this.handleSemSelectClick}
                     >
                             <Button rightIcon="caret-down"
-                                    text={sem ? sem.STARTDATE : "(No selection)"}
+                                    text={sem ? sem.STARTDATE.substring(0, 10) + " to " + sem.ENDDATE.substring(0, 10) : "(No selection)"}
                             />
                     </Select><br /><br />
+                    <div style={timeblock_s}>
+                        Select desired Office Hours:<br />
+                        <div style={bottom_s}>
+                            <TimePicker
+                                onChange={this.handleDayStart}
+                                showSecond={false}
+                                defaultValue={nowMoment}
+                                format={format}
+                                use12Hours
+                            />
+                            <Select
+                                items={DaysOfWeek}
+                                itemRenderer={this.dayRenderer}
+                                onItemSelect={this.handleDaySelectClick}
+                            >
+                                <Button
+                                    text={day ? day.TITLE : "(Choose Day)"}
+                                />
+                            </Select>
+                            <div style={{ width: 0 }}></div>
+                        </div>
+                        <TimePicker
+                            onChange={this.handleDayEnd}
+                            showSecond={false}
+                            defaultValue={nowMoment}
+                            format={format}
+                            use12Hours
+                        />
+                        <br /><br />
+                        Location:<br /><br />
+                        <div style={bottom_s}>
+                            <InputGroup id="location" placeholder="Your Location" onChange={this.handleChangeLocation.bind(this)} />
+                            <Button
+                                onClick={this.handleAddHours.bind(this)}
+                            >
+                                Add hours
+                            </Button>
+                            <Button
+                                onClick={this.removeTimesChosen.bind(this)}
+                            >
+                                Remove hours
+                            </Button>
+                        </div>
+                    </div>
+                    <div style={days_s}>
+                        {timesChosen.map(time => (
+                            <div key={time.id} style={day_s}>
+                                { (() => {
+                                    if(time.starttime.day() === 0){
+                                        return <Tag>Su</Tag>
+                                    } else if (time.starttime.day() === 1){
+                                        return <Tag>Mo</Tag>
+                                    } else if (time.starttime.day() === 2){
+                                        return <Tag>Tu</Tag>
+                                    } else if (time.starttime.day() === 3){
+                                        return <Tag>We</Tag>
+                                    } else if (time.starttime.day() === 4){
+                                        return <Tag>Th</Tag>
+                                    } else if (time.starttime.day() === 5){
+                                        return <Tag>Fr</Tag>
+                                    } else {
+                                        return <Tag>Sa</Tag>
+                                    }
+                                })()}
+                            </div>
+                        ))}
+                    </div>
                     <input type="submit" value="Submit" />
                     </form>
                     </div>
